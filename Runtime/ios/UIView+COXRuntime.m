eb83fab3cee6f0e0077d1db92fd46b0b7f224ff1
PonyCui <cuis@vip.qq.com>
2016-12-16 18:55:06 +0800

    Improve cox_layoutSubviews.

--- Runtime/ios/UIView+COXRuntime.m
+++ Runtime/ios/UIView+COXRuntime.m
@@ -15,40 +15,40 @@
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        Class class = [self class];
-        SEL originalSelector = @selector(layoutSubviews);
-        SEL swizzledSelector = @selector(cox_layoutSubviews);
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-        BOOL didAddMethod =
-        class_addMethod(class,
-                        originalSelector,
-                        method_getImplementation(swizzledMethod),
-                        method_getTypeEncoding(swizzledMethod));
-        
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                                swizzledSelector,
-                                method_getImplementation(originalMethod),
-                                method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
+      Class class = [self class];
+      SEL originalSelector = @selector(layoutSubviews);
+      SEL swizzledSelector = @selector(cox_layoutSubviews);
+      Method originalMethod = class_getInstanceMethod(class, originalSelector);
+      Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+      BOOL didAddMethod = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod),
+                                          method_getTypeEncoding(swizzledMethod));
+
+      if (didAddMethod) {
+          class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod),
+                              method_getTypeEncoding(originalMethod));
+      } else {
+          method_exchangeImplementations(originalMethod, swizzledMethod);
+      }
     });
 }
 
 - (void)cox_layoutSubviews {
+    static int kOldBoundsTag;
     [self cox_layoutSubviews];
+    if (CGSizeEqualToSize([objc_getAssociatedObject(self, &kOldBoundsTag) CGSizeValue], self.bounds.size)) {
+        return;
+    }
     UIView *previous;
     for (UIView *subview in self.subviews) {
         if (subview.cox_constraint != nil && !subview.cox_constraint.disabled) {
-            subview.frame = [subview.cox_constraint requestFrameWithMyView:subview
-                                                                 superview:self
-                                                              previousView:previous];
+            subview.frame =
+                [subview.cox_constraint requestFrameWithMyView:subview superview:self previousView:previous];
         }
         [subview automaticallyAdjustsTopSpace];
         previous = subview;
     }
+    objc_setAssociatedObject(self, &kOldBoundsTag, [NSValue valueWithCGSize:self.bounds.size],
+                             OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 - (CGSize)cox_intrinsicContentSize {
@@ -62,7 +62,8 @@ static int kAutomaticallyAdjustsSpace;
 }
 
 - (void)setCox_automaticallyAdjustsSpace:(BOOL)cox_automaticallyAdjustsSpace {
-    objc_setAssociatedObject(self, &kAutomaticallyAdjustsSpace, @(cox_automaticallyAdjustsSpace), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    objc_setAssociatedObject(self, &kAutomaticallyAdjustsSpace, @(cox_automaticallyAdjustsSpace),
+                             OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 - (void)automaticallyAdjustsTopSpace {
